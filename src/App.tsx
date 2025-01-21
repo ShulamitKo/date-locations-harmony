6c750da0e5d1d592e1fe8d7adb580dac3ed7d6bb
ShulamitKo <shoolamit09961@gmail.com>
2025-01-21 20:00:33 +0200

    גרסא 8 - התקנת האנליטיקס של וורסל

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 import HomePage from "./pages/HomePage";
 import SpotDetails from "./pages/SpotDetails";
 import Admin from "./pages/Admin";
@@ -25,6 +26,7 @@ const App = () => (
         </Routes>
         <Footer />
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
